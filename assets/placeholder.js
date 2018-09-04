103eaa7578ac3ebf3c583d81313216aa70047f1e
David Torrijos <david@mendesaltaren.com>
2018-09-04 15:03:26 +0200

    Develop of temporal placeholder

---
+++ assets/placeholder.js
@@ -0,0 +1,41 @@
+function updateTime() {
+  var countDownDate = new Date('Sep 17, 2018 09:00:00').getTime();
+  var clock = document.querySelector('.placeholder-countdown');
+  var days = clock.querySelector('.days');
+  var hours = clock.querySelector('.hours');
+  var minutes = clock.querySelector('.minutes');
+  var seconds = clock.querySelector('.seconds');
+
+  setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+
+    var newDays = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var newHours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var newMinutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var newSeconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    days.innerHTML = newDays < 10 ? '0' + newDays : newDays;
+    hours.innerHTML = newHours < 10 ? '0' + newHours : newHours;
+    minutes.innerHTML = newMinutes < 10 ? '0' + newMinutes : newMinutes;
+    seconds.innerHTML = newSeconds < 10 ? '0' + newSeconds : newSeconds;
+  }, 1000)
+};
+
+function showElements() {
+  var hiddenElements = document.querySelectorAll('.hidden');
+  var interval = 500;
+
+  [].forEach.call(hiddenElements, function(element) {
+    setTimeout(function () {
+      element.classList.remove('hidden');
+    }, interval);
+
+    interval = interval + 250;
+  });
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+  updateTime();
+  showElements();
+})
